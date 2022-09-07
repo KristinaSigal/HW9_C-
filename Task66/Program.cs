688f716d6037d1f10282bb270110b1497af2a8e3
kristina.sigal <kulrikh@gmail.com>
2022-09-08 00:56:36 +0300

    Resave HW9

---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое целое число: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе положительное число: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+int SumNumberBetween(int m, int n)
+{
+    if (m > n)
+    {
+        int temp = n;
+        n = m;
+        m = temp;
+    }
+    if (m < n)
+    {
+        return m + SumNumberBetween(m + 1, n);
+    }
+    return m;
+}
+Console.WriteLine(SumNumberBetween(firstnumber, secondnumber));
---
+++ Task68/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью
+// рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+bool mistake = false;
+int m;
+int n;
+
+do
+{
+    Console.WriteLine("Введите первое целое положительное число m: ");
+    m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе целое положительное число n: ");
+    n = Convert.ToInt32(Console.ReadLine()); 
+    mistake = n <0 || n>5 || m<0|| m>5 || (n == 1 && m > 4) || (n >1 && m > 3);
+    if (mistake) Console.WriteLine("Ошибка ввода, повторите попытку.");
+} while (mistake);
+
+void F(int m1, int n1)
+{
+        int Akkerman(int m1, int n1)
+        {
+            if (m1 == 0) return n1 + 1;
+            if (m1 > 0 && n1 == 0) return Akkerman(m1 - 1, 1);
+            if (m1 > 0 && n1 > 0) return Akkerman(m1 - 1, Akkerman(m1, n1 - 1));
+            return 0;
+        }
+    Console.WriteLine(Akkerman(m1, n1));
+}
+
+F(m, n);
